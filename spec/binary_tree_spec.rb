b306fbd213d2601b7837cdf3b21ff6d2870405bd
Dan Garland <dan@wegotcoders.com>
2017-10-24 17:03:04 +0100

    Using Minitest instead of rspec

--- spec/binary_tree_spec.rb
+++ spec/binary_tree_spec.rb
@@ -1,4 +1,6 @@
-require 'spec_helper'
+require 'minitest/autorun'
+require './lib/binary_tree'
+require './lib/node'
 
 describe BinaryTree do
 
@@ -14,44 +16,46 @@ describe BinaryTree do
     end
 
     it "should insert nodes in alphabetical order" do
-      @tree.root.data.should eq("Dan")
-      @tree.root.left.data.should eq("Barry")
-      @tree.root.right.data.should eq("Ted")
-      @tree.root.right.left.data.should eq("Daniel")
-      @tree.root.left.left.data.should eq("Alice")
-      @tree.root.left.left.right.data.should eq("Andy")
-      @tree.root.right.left.right.data.should eq("Sally")
+      @tree.root.data.must_equal("Dan")
+      @tree.root.left.data.must_equal("Barry")
+      @tree.root.right.data.must_equal("Ted")
+      @tree.root.right.left.data.must_equal("Daniel")
+      @tree.root.left.left.data.must_equal("Alice")
+      @tree.root.left.left.right.data.must_equal("Andy")
+      @tree.root.right.left.right.data.must_equal("Sally")
     end
 
-    context "next steps..." do
+    describe "next steps..." do
       before do
-        pending "TODO attempt these next tests yourself...!"
+        skip "TODO attempt these next tests yourself...!"
       end
 
       describe "nodes" do
-        it "should count the number of nodes" do
-          @tree.nodes.length.should eq(7)
+        it "should find all the nodes" do
+          @tree.nodes.length.must_equal(7)
+          @tree.nodes.map(&:data).must_equal %w(Dan Barry Ted Daniel Alice Andy Sally)
         end
       end
 
       describe "leaves" do
-        it "should count the number of leaf nodes" do
-          @tree.leaves.length.should eq(2)
+        it "should find the nodes with no left or right value" do
+          @tree.leaves.length.must_equal(2)
+          @tree.leaves.map(&:data).must_equal %w(Alice Sally)
         end
       end
 
       describe "include?" do
         it "should find a Node by name" do
-          @tree.include?('Sally').should be_true
-          @tree.include?('Brian').should be_false
+          @tree.include?('Sally').must_equal true
+          @tree.include?('Brian').must_equal false
         end
       end
 
       describe "find" do
         it "should return the node with a given name" do
-          @tree.find("Ted").should_not be_nil
-          @tree.find("Ted").left.data.should eq("Daniel")
-          @tree.find("Ted").left.right.data.should eq("Sally")
+          @tree.find("Ted").wont_equal nil
+          @tree.find("Ted").left.data.must_equal("Daniel")
+          @tree.find("Ted").left.right.data.must_equal("Sally")
         end
       end
 
@@ -61,12 +65,12 @@ describe BinaryTree do
         end
 
         it "should remove a node" do
-          @tree.include?('Ted').should be_false
+          @tree.include?('Ted').must_equal false
         end
 
         it "should replace the node with its left child" do
-          @tree.root.right.data.should eq("Daniel")
-          @tree.root.right.right.data.should eq("Sally")
+          @tree.root.right.data.must_equal("Daniel")
+          @tree.root.right.right.data.must_equal("Sally")
         end
       end
     end
